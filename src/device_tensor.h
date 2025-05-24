8370f464cad1a3ae6d759d562cf0ee53951fb348
tornikeo <tornikeonoprishvili@gmail.com>
2025-05-24 17:49:50 +0200

    move towards fixed size arrs to reduce register pressure

--- src/device_tensor.h
+++ src/device_tensor.h
@@ -123,23 +123,10 @@ class device_tensor3d_t
 {
 public:
   const int dim1, dim2, dim3;
-  const bool own;
   T * data;
   __device__ device_tensor3d_t() : dim1(0), dim2(0), dim3(0), data(nullptr) {}
-  __device__ device_tensor3d_t(int dim1, int dim2, int dim3) : dim1(dim1), dim2(dim2), dim3(dim3), own(true)
-  {
-    // Allocate memory for the tensor
-    this->data = (T *)xcalloc(dim1 * dim2 * dim3 * sizeof(T));
-  }
-  __device__ device_tensor3d_t(int dim1, int dim2, int dim3, T *data) : dim1(dim1), dim2(dim2), dim3(dim3), data(data), own(false) {}
-  
-  __device__ ~device_tensor3d_t()
-  {
-    if(own)
-      free(data);
-  }
 
-  /* Proxy for transposed indexing */
+  __device__ device_tensor3d_t(int dim1, int dim2, int dim3, T *data) : dim1(dim1), dim2(dim2), dim3(dim3), data(data) {}
   
   __device__ device_tensor3d_t(const device_tensor3d_t &other)
   {
@@ -151,6 +138,112 @@ public:
   {
     /* Warn on use, we shouldn't need this! */
     printf("Warning: device_tensor3d_t assignment operator called.\n");
+    assert(false && "Assignment operator should not be used!");
+    return *this;
+  }
+
+  __device__ inline T &operator()(int k, int j, int i, char )
+  {    
+    #ifndef NDEBUG
+    if(i < 1 || i > dim1 || j < 1 || j > dim2 || k < 1 || k > dim3)
+    {
+      printf("FORTRAN indexing error: (%i, %i, %i) out of bounds for tensor of size (%i, %i, %i)\n", k, j, i, dim3, dim2, dim1);
+      assert(false);
+    }
+    assert(i >= 1 && i <= dim1 && j >= 1 && j <= dim2 && k >= 1 && k <= dim3);
+    #endif
+    return data[(i - 1) * dim2 * dim3 + (j - 1) * dim3 + (k - 1)];
+  }
+
+  __device__ inline T &operator()(int i, int j, int k)
+  {
+    #ifndef NDEBUG
+    if(i < 0 || i >= dim1 || j < 0 || j >= dim2 || k < 0 || k >= dim3)
+    {
+      printf("Indexing error: (%i, %i, %i) out of bounds for tensor of size (%i, %i, %i)\n", i, j, k, dim1, dim2, dim3);
+      assert(false);
+    }
+    assert(i >= 0 && i < dim1 && j >= 0 && j < dim2 && k >= 0 && k < dim3);
+    #endif
+    return data[i * dim2 * dim3 + j * dim3 + k];
+  }
+
+  __device__ inline const T &operator()(int k, int j, int i, char ) const
+  {
+    #ifndef NDEBUG
+    if(i < 1 || i > dim1 || j < 1 || j > dim2 || k < 1 || k > dim3)
+    {
+      printf("FORTRAN indexing error: (%i, %i, %i) out of bounds for tensor of size (%i, %i, %i)\n", k, j, i, dim3, dim2, dim1);
+      assert(false);
+    }
+    assert(i >= 1 && i <= dim1 && j >= 1 && j <= dim2 && k >= 1 && k <= dim3);
+    #endif
+    return data[(i - 1) * dim2 * dim3 + (j - 1) * dim3 + (k - 1)];
+  }
+
+  __device__ inline const T &operator()(int i, int j, int k) const
+  {
+    #ifndef NDEBUG
+    if (i < 0 || i >= dim1 || j < 0 || j >= dim2 || k < 0 || k >= dim3)
+    {
+      printf("Indexing error: (%i, %i, %i) out of bounds for tensor of size (%i, %i, %i)\n", i, j, k, dim1, dim2, dim3);
+      assert(false);
+    }
+    assert(i >= 0 && i < dim1 && j >= 0 && j < dim2 && k >= 0 && k < dim3);
+    #endif
+    return data[i * dim2 * dim3 + j * dim3 + k];
+  }
+  __device__ inline void fill(const T &value)
+  {
+    const int size = dim1 * dim2 * dim3;
+    for (int i = 0; i < size; ++i)
+    {
+      data[i] = value;
+    }
+  }
+  __device__ inline void print() const
+  {
+    printf("(%i, %i, %i)\n", dim1, dim2, dim3);
+    printf("[");
+    for (int i = 0; i < dim1; ++i)
+    {
+      printf("[\n");
+      for (int j = 0; j < dim2; ++j)
+      {
+        printf("[");
+        for (int k = 0; k < dim3; ++k)
+        {
+          printf("%f, ", static_cast<double>(data[i * dim2 * dim3 + j * dim3 + k]));
+        }
+        printf("], \n");
+      }
+      printf("], ");
+    }
+    printf("]\n");
+  }
+};
+
+template <typename T, int D1, int D2, int D3>
+class device_tensor3d_fixed_t
+{
+  public:
+  static constexpr int dim1 = D1;
+  static constexpr int dim2 = D2;
+  static constexpr int dim3 = D3;
+  T * data;
+  __device__ device_tensor3d_fixed_t() : data(nullptr) {}
+  __device__ device_tensor3d_fixed_t(T *data) : data(data) {}
+
+  __device__ device_tensor3d_fixed_t(const device_tensor3d_fixed_t &other)
+  {
+    // Warn on use, we shouldn't need this!
+    printf("Warning: device_tensor3d_t copy constructor called.\n");
+    assert(false && "Copy constructor should not be used!");
+  }
+  __device__ device_tensor3d_fixed_t &operator=(const device_tensor3d_fixed_t &other)
+  {
+    /* Warn on use, we shouldn't need this! */
+    printf("Warning: device_tensor3d_fixed_t assignment operator called.\n");
     assert(false && "Assignment operator should not be used!");
     return *this;
   }
